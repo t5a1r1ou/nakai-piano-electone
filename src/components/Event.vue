715b117b055b2500e77293663bbbe731cecd5284
Shutto <baw.keepon69@gmail.com>
2019-03-21 22:57:59 +0900

    images minified

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -3,21 +3,21 @@
   <div class="contents-block inner">
     <h2 class="contents-title greet-title">イベント</h2>
     <p class="contents-sub">発表会</p>
-    <div class="contents-img-block_2 flexbox">
+    <div class="event-img-box">
       <img src="../assets/happyoukai1.png" alt="発表会写真" class="event-img">
       <img src="../assets/happyoukai6.png" alt="発表会写真" class="event-img">
-    </div>
       <img src="../assets/happyoukai3.png" alt="発表会写真" class="event-img">
+    </div>
     <p class="contents-detail event-detail">年１回夏頃に発表会があります。<br>
       生徒の皆さんは、ソロ演奏、アンサンブル演奏、フィナーレの部隊に参加します。</p>
     <p class="contents-sub">大人の発表会</p>
-    <div class="event-img-1">
+    <div class="event-img-box">
       <img src="../assets/happyoukai5.png" alt="大人の発表会写真" class="event-img">
     </div>
     <p class="contents-detail event-detail">大人の生徒さんだけの発表会です。和やかな雰囲気の発表会で、途中にケーキをたべるティータイムもあります。<br>
     ピアノ・エレクトーンだけでなく、歌や他の楽器での出演もあります。</p>
     <p class="contents-sub">ミュージックパーティー</p>
-    <div class="event-img-1">
+    <div class="event-img-box">
       <img src="../assets/happyoukai4.png" alt="発表会写真" class="event-img">
     </div>
     <p class="contents-detail event-detail">年１回冬ごろにあるミニコンサートです。<br>
@@ -69,5 +69,9 @@ $ssp :700px;
   margin-bottom: 3em;
 }
 
+.event-img-box {
+  width: 50%;
+  margin: 0 auto;
+}
 
 </style>
