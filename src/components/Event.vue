8a441b5b54ee21c306eccc23523a6ef61fe42e71
Shutto <baw.keepon69@gmail.com>
2019-03-21 22:53:31 +0900

    bit correct

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -11,11 +11,15 @@
     <p class="contents-detail event-detail">年１回夏頃に発表会があります。<br>
       生徒の皆さんは、ソロ演奏、アンサンブル演奏、フィナーレの部隊に参加します。</p>
     <p class="contents-sub">大人の発表会</p>
-    <img src="../assets/happyoukai5.png" alt="大人の発表会写真" class="event-img">
+    <div class="event-img-1">
+      <img src="../assets/happyoukai5.png" alt="大人の発表会写真" class="event-img">
+    </div>
     <p class="contents-detail event-detail">大人の生徒さんだけの発表会です。和やかな雰囲気の発表会で、途中にケーキをたべるティータイムもあります。<br>
     ピアノ・エレクトーンだけでなく、歌や他の楽器での出演もあります。</p>
     <p class="contents-sub">ミュージックパーティー</p>
-    <img src="../assets/happyoukai4.png" alt="発表会写真" class="event-img">
+    <div class="event-img-1">
+      <img src="../assets/happyoukai4.png" alt="発表会写真" class="event-img">
+    </div>
     <p class="contents-detail event-detail">年１回冬ごろにあるミニコンサートです。<br>
     初めて人前で演奏するデビューの生徒さんや、兄弟、姉妹、お友達同士でのアンサンブル、連弾でも参加していただけます。</p>
     <p class="contents-sub">コンクール</p>
