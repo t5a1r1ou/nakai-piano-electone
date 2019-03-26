a94e45b086b958ca8268ab907320c6c75955d55b
Shutto <baw.keepon69@gmail.com>
2019-03-26 20:17:43 +0900

    map update

--- src/components/Guide.vue
+++ src/components/Guide.vue
@@ -27,9 +27,9 @@
     </div>
     <div class="contents-block inner">
       <h2 class="contents-title">アクセス</h2>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2178.9276458076865!2d135.59969799225624!3d34.73958039621953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e1d88b716ffd%3A0xcb729731faeb3a17!2z44CSNTcxLTAwNzgg5aSn6Ziq5bqc6ZaA55yf5biC5bi455uk55S6!5e0!3m2!1sja!2sjp!4v1552725455156" frameborder="0" allowfullscreen></iframe>
-      <p class="contents-detail">門真市常盤町（京阪大和田駅から徒歩１０分）</p>
-      <p class="contents-detail">大和田小学校区　常盤温泉近く</p>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.626447585814!2d135.5962989151785!3d34.73980918863047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e1d854fc30f5%3A0xfb451dcebf2b12c9!2z44CSNTcxLTAwNzgg5aSn6Ziq5bqc6ZaA55yf5biC5bi455uk55S677yR77yZ4oiS77yR77yR!5e0!3m2!1sja!2sjp!4v1553598922864" frameborder="0" allowfullscreen></iframe>
+      <p class="contents-detail">大阪府門真市常盤町19-11（京阪大和田駅から徒歩１０分）</p>
+      <p class="contents-detail">大和田小学校区 常盤温泉近く</p>
     </div>
     <router-link to="/contact" class="foot-button">お問い合わせはこちら</router-link>
     </div>
