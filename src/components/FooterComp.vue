1ebd5a8bc1b0bc051448e4ee0a2a9214db9d61d7
Shutto <baw.keepon69@gmail.com>
2020-10-17 00:45:58 +0900

    add instagram link

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,22 +1,30 @@
 <template>
   <footer>
-    <p class="footer-tel">お電話でのお問い合わせ：<a href="tel:072-882-8961">072-882-8961</a></p>
-    <p class="footer-mail">webからのお問い合わせ：<router-link to="/contact">こちら</router-link></p>
+    <a href="https://www.instagram.com/nakaipiano_electone/">
+      <img src="../assets/instagram.png" alt="インスタグラムリンク" class="footer-insta" />
+    </a>
+    <p class="footer-tel">
+      お電話でのお問い合わせ：
+      <a href="tel:072-882-8961">072-882-8961</a>
+    </p>
+    <p class="footer-mail">
+      webからのお問い合わせ：
+      <router-link to="/contact">こちら</router-link>
+    </p>
   </footer>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-$tab :1100px;
-$sp :768px;
-$ssp : 700px;
+$tab: 1100px;
+$sp: 768px;
+$ssp: 700px;
 
 @mixin tab {
-  @media only screen and (max-width: ($tab)){
+  @media only screen and (max-width: ($tab)) {
     @content;
   }
 }
@@ -48,26 +56,30 @@ footer {
     margin-right: 2em;
     &::after {
       margin-left: 2em;
-      content: ' |';
+      content: " |";
     }
   }
 }
 .footer-tel,
 .footer-mail {
   margin-left: 3em;
-  padding-top: 3em;
+  padding-top: 1em;
   text-align: left;
 }
 
 .footer-mail {
-  padding-bottom: 3em;
+  padding-bottom: 1em;
+}
+
+.footer-insta {
+  width: 10%;
+  margin: 2em auto 0;
 }
 
 a[href^="tel:"] {
-		pointer-events: none;
-    @include sp {
-      pointer-events: auto;
-    }
-	}
-
+  pointer-events: none;
+  @include sp {
+    pointer-events: auto;
+  }
+}
 </style>
